7d0a75613df6821eb9b5b45d5e0f80cafa33f879
Archana <karchanareddy16@gmail.com>
2025-03-22 15:25:00 +0530

    final

---
+++ gradio_app.py
@@ -0,0 +1,63 @@
+# if you dont use pipenv uncomment the following:
+# from dotenv import load_dotenv
+# load_dotenv()
+
+#VoiceBot UI with Gradio
+import os
+import gradio as gr
+from pydub import AudioSegment
+
+from brain_of_the_doctor import encode_image, analyze_image_with_query
+from voice_of_the_patient import record_audio, transcribe_with_groq
+from voice_of_the_doctor import text_to_speech_with_gtts, text_to_speech_with_elevenlabs
+
+#load_dotenv()
+
+system_prompt="""You have to act as a professional doctor, i know you are not but this is for learning purpose. 
+            What's in this image?. Do you find anything wrong with it medically? 
+            If you make a differential, suggest some remedies for them. Donot add any numbers or special characters in 
+            your response. Your response should be in one long paragraph. Also always answer as if you are answering to a real person.
+            Donot say 'In the image I see' but say 'With what I see, I think you have ....'
+            Dont respond as an AI model in markdown, your answer should mimic that of an actual doctor not an AI bot, 
+            Keep your answer concise (max 2 sentences). No preamble, start your answer right away please"""
+
+def convert_mp3_to_wav(mp3_path, wav_path):
+    audio = AudioSegment.from_mp3(mp3_path)
+    audio.export(wav_path, format="wav")
+
+def process_inputs(audio_filepath, image_filepath):
+    speech_to_text_output = transcribe_with_groq(GROQ_API_KEY=os.environ.get("GROQ_API_KEY"), 
+                                                 audio_filepath=audio_filepath,
+                                                 stt_model="whisper-large-v3")
+
+    # Handle the image input
+    if image_filepath:
+        doctor_response = analyze_image_with_query(query=system_prompt+speech_to_text_output, encoded_image=encode_image(image_filepath), model="llama-3.2-11b-vision-preview")
+    else:
+        doctor_response = "No image provided for me to analyze"
+
+    mp3_path = "final.mp3"
+    wav_path = "final.wav"
+    text_to_speech_with_elevenlabs(input_text=doctor_response, output_filepath=mp3_path)
+    convert_mp3_to_wav(mp3_path, wav_path)
+
+    return speech_to_text_output, doctor_response, wav_path
+
+# Create the interface
+iface = gr.Interface(
+    fn=process_inputs,
+    inputs=[
+        gr.Audio(sources=["microphone"], type="filepath"),
+        gr.Image(type="filepath")
+    ],
+    outputs=[
+        gr.Textbox(label="Speech to Text"),
+        gr.Textbox(label="Doctor's Response"),
+        gr.Audio("final.wav")
+    ],
+    title="AI Doctor with Vision and Voice"
+)
+
+iface.launch(debug=True)
+
+#http://127.0.0.1:7860
